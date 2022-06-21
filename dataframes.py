51f970fe0c03c2b6fac46bed04acca7d625f5282
Annapoorni S <annapoornisanjeev@gmail.com>
2022-06-21 13:00:41 +0900

    Locate Named Indexes

--- dataframes.py
+++ dataframes.py
@@ -3,4 +3,4 @@ data = {"Subjects":["Maths", "Science", "English"],
 "Duration_in_hrs": [1, 2, 0.5]}
 df = pd.DataFrame(data, index = ["first period", "second period", "third period"])
 print(df)
-#print(df.loc[[0, 1]])
+print(df.loc["second period"])
