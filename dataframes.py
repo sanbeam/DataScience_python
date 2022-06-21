a870f0c96e12774b658e65b1c41759e335f841d3
Annapoorni S <annapoornisanjeev@gmail.com>
2022-06-21 12:58:20 +0900

    Changed customised index

--- dataframes.py
+++ dataframes.py
@@ -1,6 +1,6 @@
 import pandas as pd
-data = {"periods":["Maths", "Science", "English"],
-"duration_in_hrs": [1, 2, 0.5]}
-df = pd.DataFrame(data)
-#print(df)
+data = {"Subjects":["Maths", "Science", "English"],
+"Duration_in_hrs": [1, 2, 0.5]}
+df = pd.DataFrame(data, index = ["first period", "second period", "third period"])
+print(df)
 #print(df.loc[[0, 1]])
--- test.py
+++
@@ -1,1 +0,0 @@
-print("Git hub testing......")
