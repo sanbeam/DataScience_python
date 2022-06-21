2e0b206c3d2461c160a939edaa2898ff19f098a7
Annapoorni S <annapoornisanjeev@gmail.com>
2022-06-21 12:54:17 +0900

    Learnt how to ake dataframe and select rows

--- dataframes.py
+++ dataframes.py
@@ -2,6 +2,5 @@ import pandas as pd
 data = {"periods":["Maths", "Science", "English"],
 "duration_in_hrs": [1, 2, 0.5]}
 df = pd.DataFrame(data)
-print(df)
-
-print(df.loc[0])
+#print(df)
+#print(df.loc[[0, 1]])
