3299113b423d9c7cb64f59272f6981ecf3df8f2f
Annapoorni S <annapoornisanjeev@gmail.com>
2022-06-21 13:47:50 +0900

    read from csv

--- dataframes.py
+++ dataframes.py
@@ -1,6 +1,11 @@
 import pandas as pd
 data = {"Subjects":["Maths", "Science", "English"],
 "Duration_in_hrs": [1, 2, 0.5]}
-df = pd.DataFrame(data, index = ["first period", "second period", "third period"])
-print(df)
-print(df.loc["second period"])
+#df = pd.DataFrame(data, index = ["first period", "second period", "third period"])
+#print(df)
+#print(df.loc["second period"])
+
+# ---------------------------------------
+
+csv_df = pd.read_csv('sample.csv', encoding= 'ISO-8859-1')
+print(csv_df)
